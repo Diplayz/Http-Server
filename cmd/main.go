2044c180daf0295c3e29d8db9a5f9822ffa357d7
Дмитрий Степанченко <Dizelis7@yandex.ru>
2025-12-09 15:10:27 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	serv := server.NewServer(logger)
+
+	if err := serv.Start(); err != nil {
+		logger.Fatal("Server error:", err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,38 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(data string) (string, error) {
+	if data == "" {
+		return "", fmt.Errorf("ошибка: пустая строка")
+	}
+
+	clean := strings.TrimSpace(data)
+	clean = strings.Trim(clean, "\n\r\t")
+
+	converter := morse.NewConverter(
+		morse.DefaultMorse,
+		morse.WithCharSeparator(" "),
+		morse.WithWordSeparator(" "),
+		morse.WithLowercaseHandling(true),
+		morse.WithHandler(morse.IgnoreHandler),
+		morse.WithTrailingSeparator(false),
+	)
+
+	textResult := converter.ToText(clean)
+	if textResult != "" && textResult != clean {
+		return textResult, nil
+	}
+
+	morseResult := converter.ToMorse(clean)
+	if morseResult != "" {
+		return morseResult, nil
+	}
+
+	return "", fmt.Errorf("ошибка конвертации")
+}
